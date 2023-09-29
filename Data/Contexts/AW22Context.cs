e31d4d50dcf2c6a74d894ab9870621708f1fe503
ilkeryolundagerek <ilkeryolundagerek@gmail.com>
2023-09-29 19:37:52 +0300

    #2

--- Data/Contexts/AW22Context.cs
+++ Data/Contexts/AW22Context.cs
@@ -195,10 +195,6 @@ public partial class AW22Context : DbContext
 
     public virtual DbSet<WorkOrderRouting> WorkOrderRoutings { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=localhost;database=AdventureWorks2022;user id=sa;password=1;TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("SQL_Latin1_General_CP1_CI_AS");
